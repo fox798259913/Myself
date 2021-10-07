bf83f43eb56974c896abf2126ebc9de2d30087b6
StanleyChen0924 <stanleychen0924@gmail.com>
2021-10-07 22:25:08 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,22 @@
+if (Hls.isSupported()) {
+  const video = document.getElementById("video");
+  const hls = new Hls();
+  hls.attachMedia(video);
+
+  hls.on(Hls.Events.MEDIA_ATTACHED, function() {
+    console.log("video and hls.js are now bound together !");
+    hls.loadSource(
+	  "https://raw.githubusercontent.com/LiuYi0526/IPTV/YTlive/PTS.m3u8"
+    );
+  });
+
+  
+  play.addEventListener("click", () => {
+    console.log("play");
+    video.play();
+  });
+  pause.addEventListener("click", () => {
+    console.log("play");
+    video.pause();
+  });
+}
