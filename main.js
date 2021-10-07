4136158787d95ca2b628d2c5da1355652e56865f
StanleyChen0924 <stanleychen0924@gmail.com>
2021-10-08 02:15:13 +0800

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ if (Hls.isSupported()) {
   hls.on(Hls.Events.MEDIA_ATTACHED, function() {
     console.log("video and hls.js are now bound together !");
     hls.loadSource(
-	  "http://1-fss24-s0.streamhoster.com/lv_uchannel/_definst_/broadcast1/chunklist.m3u8"
+	  "https://srv1.zcast.com.br/kpoptv/kpoptv/.m3u88"
     );
   });
 
