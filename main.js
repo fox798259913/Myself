efb12d9324c3f6c86237279aab5af3256fc84fca
StanleyChen0924 <stanleychen0924@gmail.com>
2021-10-07 23:01:13 +0800

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ if (Hls.isSupported()) {
   hls.on(Hls.Events.MEDIA_ATTACHED, function() {
     console.log("video and hls.js are now bound together !");
     hls.loadSource(
-	  "https://raw.githubusercontent.com/StanleyChen0924/Myself/YTlive/Music24.m3u8"
+	  "https://4gtvfreemobile-mozai.4gtv.tv/0df68Gf2eZZfeig9vWfaKfrjsVCG-GW-OCbyLHhoW0M%3d/stream2.m3u8?token=kZe4eLTn9CRCt4byOtfzpQ&expires=1628316954&refer=ZDRjZGQzODAtOTUyMi00ZDAxLTgyMzktZTMyMzFiZWUwZGIz&token1=kHxHRFjqciiDO5a-uaorqw&expires1=1628345754&y=0&vt="
     );
   });
 
