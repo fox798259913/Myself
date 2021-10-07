fb01be2545f44425cbafe9ea1df49640e760cc69
StanleyChen0924 <stanleychen0924@gmail.com>
2021-10-07 23:16:10 +0800

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ if (Hls.isSupported()) {
   hls.on(Hls.Events.MEDIA_ATTACHED, function() {
     console.log("video and hls.js are now bound together !");
     hls.loadSource(
-	  "http://58.99.33.16:1935/liveedge17/live_122_3.stream/chunklist.m3u8"
+	  "http://1-fss24-s0.streamhoster.com/lv_uchannel/_definst_/broadcast1/chunklist.m3u8"
     );
   });
 
