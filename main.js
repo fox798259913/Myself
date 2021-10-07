96a6bc3b996711f6fad85b917d95ca8aeeb1b5e0
StanleyChen0924 <stanleychen0924@gmail.com>
2021-10-07 22:59:28 +0800

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ if (Hls.isSupported()) {
   hls.on(Hls.Events.MEDIA_ATTACHED, function() {
     console.log("video and hls.js are now bound together !");
     hls.loadSource(
-	  "http://210.210.155.35:80/session/01bdbc8e-a40f-11eb-8209-c81f66f89318/dr9445/h/h20/01.m3u8"
+	  "https://raw.githubusercontent.com/StanleyChen0924/Myself/YTlive/Music24.m3u8"
     );
   });
 
