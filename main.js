672ae7d26a11ba9ca6b2225ba9964b9b707b1edc
StanleyChen0924 <stanleychen0924@gmail.com>
2021-10-12 15:34:54 +0800

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ if (Hls.isSupported()) {
   hls.on(Hls.Events.MEDIA_ATTACHED, function() {
     console.log("video and hls.js are now bound together !");
     hls.loadSource(
-	  "https://srv1.zcast.com.br/kpoptv/kpoptv/.m3u88"
+	  "https://srv1.zcast.com.br/kpoptv/kpoptv/.m3u8"
     );
   });
 
